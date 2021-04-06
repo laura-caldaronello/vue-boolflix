71f9306e7b2cac89810b610e6edd2ace983ee9dd
Laura Caldaronello <caldaronello.l@gmail.com>
2021-04-06 16:09:35 +0200

    Stelle

--- js/main.js
+++ js/main.js
@@ -10,16 +10,22 @@ var app = new Vue({
             this.search = document.getElementById('searchInput').value;
             this.search = this.search.replace(/ /g,'+');
 
+            // reset
             this.movies = [];
+
             if (this.search != '') {
                 axios
                 .get('https://api.themoviedb.org/3/search/movie?api_key=74414cb486bec78c348850cbf8bf2fc4&language=it-IT&query=' + this.search)
                 .then((got) => {
     
                     got.data.results.forEach((result) => {
+
+                        // Definisco le stelle
+                        result.stars = Math.floor(result.vote_average / 2) + 1;
+
                         this.movies.push(result);
+
                     });
-                    console.log(this.movies);
     
                 });    
             }
