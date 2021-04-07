fcd536ec986e9a8cac8f349539f0038df65e5cff
Laura Caldaronello <caldaronello.l@gmail.com>
2021-04-07 10:56:07 +0200

    Migliorata milestone 2

--- js/main.js
+++ js/main.js
@@ -1,49 +1,54 @@
 var app = new Vue({
     el: '#root',
     data: {
+        api_key: '74414cb486bec78c348850cbf8bf2fc4',
+        language: 'it-IT',
         search: '',
+        flags: ['us','it','fr','de','es'],
+        srcFirst: 'https://www.countryflags.io/',
+        srcLast: '/flat/16.png',
         found: []
     },
     methods: {
+        transformVote: function(vote) {
+            return Math.ceil(vote / 2);
+        },
+        includeFlag: function(flag) {
+            return this.flags.includes(flag);
+        },
         searching: function() {
-
-            this.search = document.getElementById('searchInput').value;
-            this.search = this.search.replace(/ /g,'+');
 
             // reset
             this.found = [];
 
             if (this.search != '') {
                 axios
-                .get('https://api.themoviedb.org/3/search/multi?api_key=74414cb486bec78c348850cbf8bf2fc4&language=it-IT&query=' + this.search)
+                .get('https://api.themoviedb.org/3/search/multi',
+                    {params: {
+                        api_key: this.api_key,
+                        language: this.language,
+                        query: this.search
+                    }}
+                )
                 .then((got) => {
     
                     got.data.results.forEach((result) => {
 
+                        // avendo fatto la chiamata "multi" posso potenzilmente tornare anche altre tipologie di informazioni
                         if (result.media_type == 'movie' || result.media_type == 'tv') {
 
                             // Definisco le stelle
-                            result.stars = Math.floor(result.vote_average / 2) + 1;
+                            result.stars = this.transformVote(result.vote_average);
     
                             // Trasformo la lingua in una nazione per essere coerente con le icone di https://www.countryflags.io
                             result.flag = result.original_language;
-                            switch (result.flag) {
-                                case 'en':
-                                    result.flag = 'us';
-                                    break;
-                                case 'da':
-                                    result.flag = 'dk';
-                                    break;
-                                case 'el':
-                                    result.flag = 'gr';
-                                    break;
+                            if (result.flag == 'en') {
+                                result.flag = 'us';
                             };
     
                             this.found.push(result);
-    
-                            console.log(result);
 
-                        }
+                        };
 
                     });
     
