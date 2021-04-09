7e3fd2c399e6adbede029ea13554e0354605a2fd
Laura Caldaronello <caldaronello.l@gmail.com>
2021-04-09 10:44:00 +0200

    Corretta milestone 6

--- js/main.js
+++ js/main.js
@@ -42,87 +42,92 @@ var app = new Vue({
             this.searchOpen = !this.searchOpen;
         },
         searching: function() {
+            
+            // se l'ultimo array del created è stato popolato
+            if (this.movieGenres.length != 0) {
 
-            // reset
-            this.found = [];
-
-            if (this.search != '') {
-                axios
-                .get('https://api.themoviedb.org/3/search/multi',
-                    {params: {
-                        api_key: this.api_key,
-                        language: this.language,
-                        query: this.search
-                    }}
-                )
-                .then((got) => {
-
-                    got.data.results.forEach((resultOriginal) => {
-                        // console.log(result);
-                        // faccio una copia del result
-                        let result = {...resultOriginal};
-
-                        // avendo fatto la chiamata "multi" posso potenzialmente ottenere anche altre tipologie di informazioni
-                        if (result.media_type == 'movie' || result.media_type == 'tv') {
-
-                            // inserisco i generi differenziando tra movie e serie. si potrebbe ottimizzare
-                            result.genre_names = [];
-                            if (result.media_type == 'movie') {
-                                this.movieGenres.forEach((genre) => {
-                                    if (result.genre_ids.includes(genre.id)) {
-                                        result.genre_names.push(genre.name);
-                                    };
-                                });
-                            }
-                            else if (result.media_type == 'tv') {
-                                this.tvGenres.forEach((genre) => {
-                                    if (result.genre_ids.includes(genre.id)) {
-                                        result.genre_names.push(genre.name);
-                                    };
-                                });
-                            }
-
-                            // da qui chiamo gli attori generalizzando le chiamate (in caso volessi successivamente chiamare altri dettagli del film)
-                            for (let i = 0; i < this.detailsRequested.length; i++) {
-                                // reset
-                                result[this.detailsPushed[i]] = [];
-                                // chiamata
-                                axios
-                                .get('https://api.themoviedb.org/3/' + result.media_type + '/' + result.id + '/' + this.detailsRequested[i] + '?api_key=' + this.api_key + '&language=' + this.language)
-                                .then((got) => {
-                                    if (this.detailsRequested[i] == 'credits') {
-                                        if (got.data.cast.length != 0) {
-                                            for (let i = 0; i < 5; i++) {
-                                                if (got.data.cast[i] != undefined) {
-                                                    result.cast5.push(got.data.cast[i]);
+                // reset
+                this.found = [];
+    
+                if (this.search != '') {
+                    axios
+                    .get('https://api.themoviedb.org/3/search/multi',
+                        {params: {
+                            api_key: this.api_key,
+                            language: this.language,
+                            query: this.search
+                        }}
+                    )
+                    .then((got) => {
+    
+                        got.data.results.forEach((resultOriginal) => {
+                            // console.log(result);
+                            // faccio una copia del result
+                            let result = {...resultOriginal};
+    
+                            // avendo fatto la chiamata "multi" posso potenzialmente ottenere anche altre tipologie di informazioni
+                            if (result.media_type == 'movie' || result.media_type == 'tv') {
+    
+                                // inserisco i generi differenziando tra movie e serie. si potrebbe ottimizzare
+                                result.genre_names = [];
+                                if (result.media_type == 'movie') {
+                                    this.movieGenres.forEach((genre) => {
+                                        if (result.genre_ids.includes(genre.id)) {
+                                            result.genre_names.push(genre.name);
+                                        };
+                                    });
+                                }
+                                else if (result.media_type == 'tv') {
+                                    this.tvGenres.forEach((genre) => {
+                                        if (result.genre_ids.includes(genre.id)) {
+                                            result.genre_names.push(genre.name);
+                                        };
+                                    });
+                                }
+    
+                                // da qui chiamo gli attori generalizzando le chiamate (in caso volessi successivamente chiamare altri dettagli del film)
+                                for (let i = 0; i < this.detailsRequested.length; i++) {
+                                    // reset
+                                    result[this.detailsPushed[i]] = [];
+                                    // chiamata
+                                    axios
+                                    .get('https://api.themoviedb.org/3/' + result.media_type + '/' + result.id + '/' + this.detailsRequested[i] + '?api_key=' + this.api_key + '&language=' + this.language)
+                                    .then((got) => {
+                                        if (this.detailsRequested[i] == 'credits') {
+                                            if (got.data.cast.length != 0) {
+                                                for (let i = 0; i < 5; i++) {
+                                                    if (got.data.cast[i] != undefined) {
+                                                        result.cast5.push(got.data.cast[i]);
+                                                    }
                                                 }
                                             }
                                         }
-                                    }
-                                });
-                            }
-
-                            // Definisco le stelle
-                            result.stars = this.transformVote(result.vote_average);
+                                    });
+                                }
     
-                            // Trasformo la lingua in una nazione per essere coerente con le icone di https://www.countryflags.io
-                            result.flag = result.original_language;
-                            if (result.flag == 'en') {
-                                result.flag = 'us';
+                                // Definisco le stelle
+                                result.stars = this.transformVote(result.vote_average);
+        
+                                // Trasformo la lingua in una nazione per essere coerente con le icone di https://www.countryflags.io
+                                result.flag = result.original_language;
+                                if (result.flag == 'en') {
+                                    result.flag = 'us';
+                                };
+    
+                                this.found.push(result);
+    
                             };
-
-                            this.found.push(result);
-
-                        };
-
-                    });
     
-                });
+                        });
+        
+                    });
+                }
+
             }
 
         }
     },
-    mounted() {
+    created() {
         
         // chiamo i generi
         axios
